a22e773907bdb2836327153c4ded21245bb43ed7
lee-17 <146800446+lee-17@users.noreply.github.com>
2026-06-22 10:59:08 +0800

    sql

--- F6-ACT/register.php
+++ F6-ACT/register.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = new mysqli("localhost", "root", "", "dogdb");
+$conn = new mysqli("localhost", "root", "", "fa6_baccay");
 
 if ($conn->connect_error) {
     die("Connection Failed: " . $conn->connect_error);
